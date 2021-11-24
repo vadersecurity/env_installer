2da1db6419fcce4f254b06f7a564f6f79f2d1c78
dualfade <dualfade@vadersecurity.com>
2021-11-24 09:48:02 -0500

    + comment out custom

--- config/wm/spectrwm/run.sh
+++ config/wm/spectrwm/run.sh
@@ -8,6 +8,6 @@
 /usr/bin/xrandr --output DP3 --right-of eDP1 --mode 3440x1440 --auto
 
 # start other shit --
-/usr/bin/picom --config ~/.config/picom/picom.conf &
-/usr/bin/feh --bg-fill ~/.wallpaper/min_skull.jpg &
-/usr/bin/xrdb -merge ~/.Xresources &
+#/usr/bin/picom --config ~/.config/picom/picom.conf &
+#/usr/bin/feh --bg-fill ~/.wallpaper/min_skull.jpg &
+#/usr/bin/xrdb -merge ~/.Xresources &
