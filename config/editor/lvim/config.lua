20aa28cf9178828d431ad50ef5cb7fa816da8786
dualfade <dualfade@vadersecurity.com>
2022-08-12 00:52:41 -0400

    + config.lua update

--- config/editor/lvim/config.lua
+++ config/editor/lvim/config.lua
@@ -8,14 +8,36 @@ an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
+-- refs --
+-- https://github.com/abzcoding/lvim
+
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "tokyonight"
-vim.g.tokyonight_style = "storm"
 
--- alt theme
--- lvim.colorscheme = "duskfox"
+-- lvim.colorscheme = "catppuccin"
+
+-- colorscheme
+-- https://github.com/EdenEast/nightfox.nvim
+lvim.colorscheme = "duskfox"
+-- lvim.colorscheme = "carbonfox"
+-- lvim.colorscheme = "terafox"
+-- lvim.colorscheme = "nordfox"
+
+require('nightfox').setup({
+  options = {
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      types = "italic,bold",
+    }
+  }
+})
+
+-- theme alt --
+-- lvim.colorscheme = "tokyonight"
+-- vim.g.tokyonight_style = "storm"
+
 lvim.line_wrap_cursor_movement = false
 vim.g.tokyonight_transparent = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
@@ -185,24 +207,25 @@ linters.setup {
     filetypes = { "typescript", "typescriptreact" },
   },
 }
--- linters.setup {
--- { command = "flake8", filetypes = { "python" } },
--- {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
--- },
--- }
+linters.setup {
+  -- { command = "flake8", filetypes = { "python" } },
+  {
+    -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "shellcheck",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    extra_args = { "--severity", "warning" },
+  },
+  {
+    command = "codespell",
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "javascript", "python" },
+  },
+}
 
 -- Additional Plugins
 lvim.plugins = {
+  { "catppuccin/nvim" },
   { "folke/tokyonight.nvim" },
   { "EdenEast/nightfox.nvim" },
   { "projekt0n/github-nvim-theme" },
