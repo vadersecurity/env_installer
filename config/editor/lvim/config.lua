99c38df229ceaa574396f17f63cf23991922842b
dualfade <dualfade@vadersecurity.com>
2022-11-06 09:24:35 -0500

    + adjust minimap to 20px w

--- config/editor/lvim/config.lua
+++ config/editor/lvim/config.lua
@@ -245,7 +245,7 @@ lvim.plugins = {
     run = "cargo install --locked code-minimap",
     cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
     config = function()
-      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_width = 20")
       vim.cmd("let g:minimap_auto_start = 1")
       vim.cmd("let g:minimap_auto_start_win_enter = 1")
     end,
