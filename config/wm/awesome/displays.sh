f8243409785757319f4f001c558025d9c1ab2142
dualfade <dualfade@vadersecurity.com>
2023-01-06 11:43:35 -0500

    +  set_multi_displays.sh updated

--- config/wm/awesome/displays.sh
+++ config/wm/awesome/displays.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-# download and adjust to your needs --
 # @dualfade
 
-/usr/bin/xrandr --output eDP1 --primary
-/usr/bin/xrandr --output eDP1 --mode 1920x1080 --rate 60
-/usr/bin/xrandr --output DP1 --left-of eDP1 --auto
-/usr/bin/xrandr --output DP3 --right-of eDP1 --mode 3440x1440 --auto
+# conected displays --
+# xrandr | grep -w connected | cut -f '1' -d ' '
+
+# laptop; primary --
+/usr/bin/xrandr --output eDP-1 --primary
+/usr/bin/xrandr --output eDP-1 --mode 1920x1080 --auto
+/usr/bin/xrandr --output eDP-1 --brightness 1.2
+
+# decgear overhead --
+# /usr/bin/xrandr --output DP-1 --left-of eDP-1 --auto
+# G: 2.0 R: 47 B: 46 G: 52
+/usr/bin/xrandr --output DP-1 --left-of eDP-1 --mode 2560x1440 --auto
+/usr/bin/xrandr --output DP-1 --brightness 1.0
+
+# Desktop main --
+/usr/bin/xrandr --output DP-3 --right-of eDP-1 --mode 3440x1440 --auto
+/usr/bin/xrandr --output DP-3 --brightness 1.0
+
+# start other shit --
+# -> yain network-manager-applet
+# required by protonvpn-cli; stupid dep  --
+# /usr/bin/nm-applet &
+# /usr/bin/feh --bg-fill ~/.wallpaper/dark-cubes-hd-wallpaper.jpg &
+/usr/bin/feh --bg-fill ~/.wallpaper/purple-9.jpg &
+/usr/bin/xrdb -merge ~/.Xresources &
